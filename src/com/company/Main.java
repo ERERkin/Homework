e2c1a814b8169dd28251eda47acfc7337707877f
ERERkin <erkin1399@gmail.com>
2020-01-16 16:58:49 +0600

    Modify master -added postgresSQL -added connect function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+    private static final String url = "jdbc:postgresql://localhost:5432/";
+    private static final String user = "postgres";
+    private static final String password = "postgres";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Main DB = new Main();
+        Connection connection = DB.connect();
+        if (connection != null) connection.close();
+    }
 
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
     }
 }
